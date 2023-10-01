f02a0d21f9a6c0a90b91ba1a36fe6439a5c0a6d1
AgrastP <agrastp@yahoo.com>
2023-10-01 10:19:44 -0400

    changes to html and JS

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,31 @@
 var searchBtn = document.getElementById("button");
 var cityInput = document.getElementById("city-input");
 var APIkey = "7d1a630efe515812d5181c337b746a9f";
+var listEl = document.getElementsByClassName("card");
 // var city;
-
 
 function getWeatherDetails(cityName, lat, lon) {
     var weather_API = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=` + APIkey + `&units=imperial`;
-
     fetch(weather_API)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            for (let i=0; i<data.list.length; i+=8){
-                var currentDayWeather = data.list[i]
+            for (let i = 0; i < data.list.length; i += 8) {
+                var currentDayWeather = data.list[i];
+              
+                console.log(currentDayWeather);
+                var date = currentDayWeather.dt_txt.split(" ")[0];
                 var wind = currentDayWeather.wind.speed;
                 var weatherCondition = currentDayWeather.weather.description;
                 var weatherIcon = `https://openweathermap.org/img/wn/${currentDayWeather.weather.icon}@2x.png`;
                 var temperature = currentDayWeather.main.temp;
-                var humidity = currentDayWeather.main.humidity;
+                var humidity = currentDayWeather.main.humidity; 
+
+                console.log(date);
+
             }
         })
-
-
         .catch(function () {
             alert("An error occured while retriving your weather forecast!")
         })
