c31f8fc7ac9397cb4c9a59563f73a5d4d40ab1bd
AgrastP <agrastp@yahoo.com>
2023-10-03 12:36:00 -0400

    added comments to files

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var cityBtn = document.querySelector(".cityButton");
 var APIkey = "7d1a630efe515812d5181c337b746a9f";
 
-
+//Uses latitude and longitude values from coordinates funtion to search API for weather
 function getWeatherDetails(cityName, lat, lon) {
     var weather_API = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=` + APIkey + `&units=imperial`;
     fetch(weather_API)
@@ -10,20 +10,21 @@ function getWeatherDetails(cityName, lat, lon) {
         })
         .then(function (data) {
 
-           renderCurrentWeather(data);
-           renderFiveDayForcast(data);
+            renderCurrentWeather(data);
+            renderFiveDayForcast(data);
         })
         .catch(function () {
             alert("An error occured while retriving your weather forecast!")
         })
 }
 
+//Renders weather for current day weather at top of webpage
 var dayOneWeatherCard = document.getElementById("weather-details");
 var dayOneIconCard = document.getElementById("icon-img");
-var dayOneWeatherConditionCard =document.getElementById("weather-condition");
+var dayOneWeatherConditionCard = document.getElementById("weather-condition");
 
-function renderCurrentWeather(data){
-    
+function renderCurrentWeather(data) {
+
     var currentCity = data.city.name;
     var dayOneDate = data.list[0].dt_txt.split(" ")[0];
     var dayOneTemp = data.list[0].main.temp;
@@ -31,9 +32,9 @@ function renderCurrentWeather(data){
     var dayOneWeatherCondition = data.list[0].weather[0].description;
     var dayOneWeatherIcon = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
     var dayOneHumidity = data.list[0].main.humidity;
-  
+
     var currentDayCard = document.createElement("p");
-    currentDayCard.innerHTML = currentCity+", \n" + dayOneDate + "<br> Temperature: " + Math.round(dayOneTemp) + " °F <br> Wind: " + dayOneWind + " mph <br> Humidity: " + dayOneHumidity + " %";
+    currentDayCard.innerHTML = currentCity + ", \n" + dayOneDate + "<br> Temperature: " + Math.round(dayOneTemp) + " °F <br> Wind: " + dayOneWind + " mph <br> Humidity: " + dayOneHumidity + " %";
     dayOneWeatherCard.appendChild(currentDayCard);
 
     var currentWeatherCondition = document.createElement("p");
@@ -41,43 +42,45 @@ function renderCurrentWeather(data){
     dayOneWeatherConditionCard.appendChild(currentWeatherCondition);
 
     var currentIcon = document.createElement("img");
-    dayOneIconCard.innerHTML = "<img src="+ dayOneWeatherIcon + ">";
+    dayOneIconCard.innerHTML = "<img src=" + dayOneWeatherIcon + ">";
     dayOneIconCard.appendChild(currentIcon);
 
-    
+
 }
 
+//Creates weather cards and renders weather for the five day forecast
+
 var fiveDayForecastCards = document.querySelector(".weather-cards");
-function renderFiveDayForcast(data){
-            
+function renderFiveDayForcast(data) {
+
     for (let i = 0; i < data.list.length; i += 8) {
-                var currentDayWeather = data.list[i];
-                var date = currentDayWeather.dt_txt.split(" ")[0];
-                var wind = currentDayWeather.wind.speed;
-                var weatherCondition = currentDayWeather.weather[0].description;
-                var weatherIcon = `https://openweathermap.org/img/wn/${currentDayWeather.weather[0].icon}@2x.png`;
-                var weatherIconSrc = "<img src="+ weatherIcon + ">"
-                var temperature = currentDayWeather.main.temp;
-                var humidity = currentDayWeather.main.humidity; 
+        var currentDayWeather = data.list[i];
+        var date = currentDayWeather.dt_txt.split(" ")[0];
+        var wind = currentDayWeather.wind.speed;
+        var weatherCondition = currentDayWeather.weather[0].description;
+        var weatherIcon = `https://openweathermap.org/img/wn/${currentDayWeather.weather[0].icon}@2x.png`;
+        var weatherIconSrc = "<img src=" + weatherIcon + ">"
+        var temperature = currentDayWeather.main.temp;
+        var humidity = currentDayWeather.main.humidity;
 
-                var fiveDays = document.createElement("div");
-                fiveDays.innerHTML = date + "<br> Temp: " + Math.round(temperature) + " °F <br> Wind: " + wind + " mph <br> Humidity: " + humidity + " % <br>" + weatherCondition + "<br><br>"+ weatherIconSrc;
-                fiveDayForecastCards.appendChild(fiveDays);
-                fiveDays.setAttribute("class", "card");
-            }  
-        }
+        var fiveDays = document.createElement("div");
+        fiveDays.innerHTML = date + "<br> Temp: " + Math.round(temperature) + " °F <br> Wind: " + wind + " mph <br> Humidity: " + humidity + " % <br>" + weatherCondition + "<br><br>" + weatherIconSrc;
+        fiveDayForecastCards.appendChild(fiveDays);
+        fiveDays.setAttribute("class", "card");
+    }
+}
 
-
+//Puts search history into local storage and creates a button for each city searched
 var pastHistory = document.querySelector("#history");
 var searchBtn = document.getElementById("button");
 
-function handleInputedCities(cityName){
+function handleInputedCities(cityName) {
     var cityName = cityInput.value.trim();
 
     const localStorageContent = localStorage.getItem('cityHistory');
 
     let cityHistory;
-    if (localStorageContent === null){
+    if (localStorageContent === null) {
         cityHistory = [];
     } else {
         cityHistory = JSON.parse(localStorageContent);
@@ -86,8 +89,8 @@ function handleInputedCities(cityName){
     cityHistory.push(cityName);
     localStorage.setItem('cityHistory', JSON.stringify(cityHistory));
 
-    for (let i = 0; i< cityHistory.length; i+=1)
-    var eachCity = cityHistory[i];
+    for (let i = 0; i < cityHistory.length; i += 1)
+        var eachCity = cityHistory[i];
 
     var cityButton = document.createElement("button");
     cityButton.innerHTML = eachCity;
@@ -96,7 +99,8 @@ function handleInputedCities(cityName){
     console.log(cityButton.textContent)
 }
 
-function searchHistory(){
+//Past search history buttons can be clicked to quickly retrieve weather forecast
+function searchHistory() {
     var cityName = cityBtn.textContent;
     console.log(cityName)
     if (!cityName) return;
@@ -114,9 +118,10 @@ function searchHistory(){
             var { name, lat, lon } = data[0];
             getWeatherDetails(name, lat, lon)
         }).catch(function (error) {
-        });   
+        });
 }
 
+//Uses inputed city name to retrieve latitude and longitude data from API
 var cityInput = document.getElementById("city-input");
 
 function getCityCoordinates(cityName) {
@@ -136,9 +141,10 @@ function getCityCoordinates(cityName) {
             var { name, lat, lon } = data[0];
             getWeatherDetails(name, lat, lon)
         }).catch(function (error) {
-        });   
+        });
 }
 
+//Buttons
 searchBtn.addEventListener("click", getCityCoordinates);
 
 cityBtn.addEventListener("click", searchHistory);
